3d61b0b08320e007de7354513585e8eb603cb692
kamneklogs <kamneklogs@gmail.com>
2020-03-28 01:08:51 -0500

    Some very bad changes

--- src/model/TuringMasterClass.java
+++ src/model/TuringMasterClass.java
@@ -31,6 +31,7 @@ public class TuringMasterClass {
 
             opsS = br.readLine();
 
+            System.out.println(opsS);
             if (opsS != null) {
 
                 runLineOp(opsS);
@@ -49,24 +50,37 @@ public class TuringMasterClass {
 
         for (int i = 0; i < ops.length; i++) {
 
+            Element current = firstElement;
+            while (current != null) {
+
+                System.out.println(current.getL());
+                current = current.getNextE();
+
+            }
+
             switch (ops[i]) {
 
                 case '0':// cabeza 0
 
                     switch (ops[i + 1]) {
 
-                        case '0':// leer
+                        case '0':// Escribir en el archivo
 
-                            write(0);
+                            writeOnFile(0);
                             i = i + 1;
 
                             break;
 
-                        case '1':
+                        case '1': // cargar desde el archivo
 
-                            read(0, ops[i + 2]);
+                            readFromFile(0, ops[i + 2]);
                             i = i + 2;
 
+                            break;
+
+                        case '2':
+                            delete(0);
+                            i = i + 1;
                             break;
 
                         default:
@@ -78,8 +92,24 @@ public class TuringMasterClass {
                 case '1':// cabeza 1
 
                     switch (ops[i + 1]) {
+
                         case '0':// leer
 
+                            writeOnFile(1);
+                            i = i + 1;
+
+                            break;
+
+                        case '1':
+
+                            readFromFile(1, ops[i + 2]);
+                            i = i + 2;
+
+                            break;
+
+                        case '2':
+                            delete(1);
+                            i = i + 1;
                             break;
 
                         default:
@@ -91,8 +121,25 @@ public class TuringMasterClass {
                 case '2':// cabeza 2
 
                     switch (ops[i + 1]) {
+
                         case '0':// leer
 
+                            writeOnFile(2);
+                            i = i + 1;
+
+                            break;
+
+                        case '1':
+
+                            readFromFile(2, ops[i + 2]);
+
+                            i = i + 2;
+
+                            break;
+
+                        case '2':
+                            delete(2);
+                            i = i + 1;
                             break;
 
                         default:
@@ -106,7 +153,7 @@ public class TuringMasterClass {
 
     }
 
-    public void write(int headN) throws IOException {
+    public void writeOnFile(int headN) throws IOException {
 
         File f = new File("data/out_turing.txt");
         PrintWriter pw = new PrintWriter(new FileWriter(f));
@@ -144,35 +191,93 @@ public class TuringMasterClass {
 
     }
 
-    public void read(int headN, char l) {
+    public void readFromFile(int headN, char l) {
 
-        switch (headN) {
+        if (firstElement == null) {
+            firstElement = new Element(l);
+            c0 = firstElement;
+            c1 = firstElement;
+            c2 = firstElement;
+            listLeght++;
+        } else {
 
-            case 0:
-            
-                break;
+            Element newE = new Element(l);
 
+            switch (headN) {
+
+                case 0:
+                    Element currentc0 = firstElement;
+                    firstElement = newE;
+                    firstElement.setNextE(currentc0.getNextE());
+                    c0 = firstElement;
+                    listLeght++;
+                    break;
+
+                case 1:
+
+                    Element currentc1 = c1;
+                    // System.out.println(currentc1.getL());
+                    Element prec1 = c1.getPrevE();
+
+                    c1 = newE;
+                    c1.setPrevE(prec1);
+                    c1.setNextE(currentc1);
+                    // System.out.println(c1.getL());
+                    if (listLeght == 2) {
+                        c2 = c1;
+                    }
+                    listLeght++;
+
+                    break;
+
+                case 2:
+                    Element currentc2 = c2;
+                    // System.out.println(currentc1.getL());
+                    c2 = newE;
+                    c2.setPrevE(currentc2);
+
+                    break;
+
+            }
         }
 
     }
 
-    public char delete(int headN) {
-        char c = ' ';
+    public void delete(int headN) {
 
-        switch (headN) {
-            case 0:
-                c = c0.getL();
-                break;
+        if (listLeght == 1) {
+            firstElement = null;
+            c0 = firstElement;
+            c1 = firstElement;
+            c2 = firstElement;
+            listLeght--;
+        } else {
+            switch (headN) {
+                case 0:
+                    c0 = firstElement.getNextE();
+                    firstElement = c1;
 
-            case 1:
-                c = c1.getL();
-                break;
-            case 2:
-                c = c2.getL();
-                break;
+                    listLeght--;
+                    break;
+
+                case 1:
+
+                    Element prec1 = c1.getPrevE();
+                    Element nexc1 = c1.getNextE();
+
+                    c1 = prec1;
+                    c1.setNextE(nexc1);
+
+                    listLeght--;
+                    break;
+                case 2:
+                    c2 = c2.getPrevE();
+                    c2.setNextE(null);
+                    listLeght--;
+                    break;
+            }
         }
 
-        return c;
     }
 
 }
